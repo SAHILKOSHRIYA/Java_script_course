a7cf9180e74e9d6a6b7b2e5a440a844dba729c46
SAHIL KOSHRIYA <134610143+SAHILKOSHRIYA@users.noreply.github.com>
2025-05-03 09:18:58 +0000

    variables_step1

---
+++ 01_basicsJS/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=1133
+let accountEmail= "Sahilkoshriya@gmail.com"
+var accountpassword="12345"
+let accountset;
+
+accountcity="Jaipur"
+
+ // accountId=2  // Assingment to the conatant variables are not allowed
+console.log(accountId);
+accountEmail="sahil@gmail.com"
+accountpassword="654123"
+accountcity="bengaluru"
+console.log(accountId);
+console.log(accountEmail);
+
+
+//  table method
+
+console.table([accountId,accountEmail,accountpassword,accountcity,accountset])
+//  prefer not use Var because of block a scope and function scope contradiction  
